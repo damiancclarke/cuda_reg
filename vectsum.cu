6954d1fd42c4d3d29b9adb2b037ccd99dc9d315a
damiancclarke <damian.c.clarke@gmail.com>
2014-01-03 06:11:31 -0300

    Updated vectadd function, but needs correction for rand_ints()

--- vectsum.cu
+++ vectsum.cu
@@ -1,32 +1,47 @@
 #include <stdio.h>
 
+#define N 512
+
 __global__ void add(int *a, int *b, int *c) {
-  *c = *a + *b;
+  c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
 }
 
+
 int main(void) {
-  int a,b,c;
+  int *a,*b,*c;
   int *dev_a, *dev_b, *dev_c;
-  int size = sizeof(int);
+  int size = N * sizeof(int);
 
   cudaMalloc( (void**)&dev_a, size);
   cudaMalloc( (void**)&dev_b, size);
   cudaMalloc( (void**)&dev_c, size);
 
-  a = 2;
-  b = 7;
+  a = (int*)malloc(size);
+  b = (int*)malloc(size);
+  c = (int*)malloc(size);
 
-  cudaMemcpy( dev_a, &a, size, cudaMemcpyHostToDevice );
-  cudaMemcpy( dev_b, &b, size, cudaMemcpyHostToDevice );
+  random_ints(a, N);
+  random_ints(b, N);
+
+  cudaMemcpy( dev_a, a, size, cudaMemcpyHostToDevice );
+  cudaMemcpy( dev_b, b, size, cudaMemcpyHostToDevice );
 
   
-  add<<<1,1>>> (dev_a,dev_b,dev_c);
+  add<<<N,1>>> (dev_a,dev_b,dev_c);
 
-  cudaMemcpy(&c, dev_c, size, cudaMemcpyDeviceToHost );
+  cudaMemcpy(c, dev_c, size, cudaMemcpyDeviceToHost );
 
+  free(a); free(b); free(c);
   cudaFree( dev_a );
   cudaFree( dev_b );
   cudaFree( dev_c );
 
   return 0;
+}
+
+void random_ints(int* a, int N)
+{
+  int i;
+  for (i = 0; i < N; ++i)
+    a[i] = rand();
 }
